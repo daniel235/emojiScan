fa5dc67b94e0609f2e15075a6b101b48e8e38931
Raven Solis <daduncan298@gmail.com>
2018-11-03 21:46:25 -0500

    added git repo for rc car

--- network/rasberryPi/server.py
+++ network/rasberryPi/server.py
@@ -18,11 +18,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(True)
 conn, addr = s.accept()
-'''
-length = recvall(conn, 16)
-stringData = recvall(conn, int(length))
-data = numpy.fromstring(stringData, dtype='uint8')
-'''
+'''while True:
+    length = recvall(conn, 16)
+    stringData = recvall(conn, int(length))
+    data = numpy.fromstring(stringData, dtype='uint8')
+    '''
 s.close()
 '''
 decimg = cv2.imdecode(data, 1)
