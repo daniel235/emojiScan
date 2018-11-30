c2c2e273c4915ba84b27d9d21a12a557d1d8794e
daniel235 <daduncan298@gmail.com>
2018-11-29 21:21:54 -0600

    update

--- network/dqn.py
+++ network/dqn.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 sys.path.append("../game/car.py")
-sys.path.insert("../game/track.py")
+sys.path.append("../game/track.py")
 import random
 from skimage import color
 from collections import deque
