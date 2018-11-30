2bbdce2c41d1c098f89a356304f59d6e138d1339
daniel235 <daduncan298@gmail.com>
2018-11-30 14:58:16 -0600

    update

--- game/track.py
+++ game/track.py
@@ -34,43 +34,47 @@ class track:
 
         return 0
 
-    def update_input(self):
-        pygame.event.pump()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                return True
+    def update_input(self, vm=False):
+        if vm is False:
+            pygame.event.pump()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    return True
 
-            #input
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
-                self.car.control(0)
+                #input
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+                    self.car.control(0)
 
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
-                self.car.control(2)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                    self.car.control(2)
 
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
-                self.car.control(1)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+                    self.car.control(1)
 
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
-                self.car.control(3)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+                    self.car.control(3)
 
         return False
 
 
-    def update_screen(self):
-        self.screen.fill((0, 100, 30))
-        #x y width height
-        pygame.draw.rect(self.screen, (255, 255, 255), [400, 300, 130, 500])
-        pygame.draw.rect(self.screen, (255, 255, 255), [400, 300, 350, 100])
-        pygame.draw.rect(self.screen, (255, 255, 255), [620, 0, 130, 400])
-        pygame.draw.circle(self.screen, (100, 0, 0), (650, 10), 20)
-        self.drawCar()
-        pygame.display.flip()
+    def update_screen(self, vm=False):
+        if vm is False:
+            self.screen.fill((0, 100, 30))
+            #x y width height
+            pygame.draw.rect(self.screen, (255, 255, 255), [400, 300, 130, 500])
+            pygame.draw.rect(self.screen, (255, 255, 255), [400, 300, 350, 100])
+            pygame.draw.rect(self.screen, (255, 255, 255), [620, 0, 130, 400])
+            pygame.draw.circle(self.screen, (100, 0, 0), (650, 10), 20)
+            self.drawCar()
+            pygame.display.flip()
+        else:
+            pass
 
 
     def drawCar(self):
